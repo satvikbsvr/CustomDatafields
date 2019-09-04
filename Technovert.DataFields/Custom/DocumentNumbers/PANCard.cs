976145b27e9f82282f7f726226957dba3e4aa177
satvikbsvr <53758002+satvikbsvr@users.noreply.github.com>
2019-09-04 18:05:56 +0530

    Added type for pan card

---
+++ Technovert.DataFields/Custom/DocumentNumbers/PANCard.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Technovert.DataFields.Custom.DocumentNumbers
+{
+    public class PANCard : StringDataField
+    {
+        public PANCard() : base() { }
+        public PANCard(string val) : base(val) { }
+
+        public override void InitDataField()
+        {
+            this.Pattern = "^([A-Z]){5}([0-9]){4}([A-Z]){1}?$";
+            this.Description = "Indian PAN card details";
+            IsPatternValidationRequired = true;
+        }
+    }
+}
