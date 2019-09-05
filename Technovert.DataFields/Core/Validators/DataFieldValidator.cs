f82eba576c0b73e0908883cc932ee3fd48e97bbd
satvikbsvr <53758002+satvikbsvr@users.noreply.github.com>
2019-09-05 14:52:48 +0530

    Added overload for checkvalidity in validator

--- Technovert.DataFields/Core/Validators/DataFieldValidator.cs
+++ Technovert.DataFields/Core/Validators/DataFieldValidator.cs
@@ -4,17 +4,25 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Technovert.DataFields.Infrastructure;
+using Technovert.DataFields.Interfaces;
 
 namespace Technovert.DataFields.Validators
 {
     public static class DataFieldValidator
     {
-        public static bool CheckValidity(object o, Enums.DataFieldType type)
+        public static bool CheckValidity(object value, Enums.DataFieldType type)
         {
             var dataField = DataFieldFactory.Create(type);
-            dataField.Value = o;
+            dataField.Value = value;
             return dataField.IsValid;
         }
+
+        public static bool CheckValidity<T>(T value, bool isRequired, bool isPatternValidationRequired,string pattern)
+        {
+            var dataField = new DataField<T>(value, isRequired, isPatternValidationRequired, pattern);
+            return dataField.IsValid;
+        }
+
     }
 
 
