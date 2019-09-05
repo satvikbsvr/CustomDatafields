e93d14f0e9d0dcf2af0add4ba8521b5f346877ab
Poojitha <pujitha.t@technovert.com>
2019-09-05 16:31:02 +0530

    Added Custom Activity project

---
+++ Technovert.DataFields.CustomActivities/Validation.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Activities;
+using System.ComponentModel;
+using static Technovert.DataFields.Infrastructure.Enums;
+using static Technovert.DataFields.Validators.DataFieldValidator;
+
+namespace Technovert.DataFields.CustomActivities
+{
+    public class Validation : CodeActivity
+    {
+        [Category("Input")]
+        [RequiredArgument]
+        public InArgument<string> Type { get; set; }
+
+        [Category("Input")]
+        [RequiredArgument]
+        public InArgument<string> Value { get; set; }
+
+        [Category("Output")]
+        public OutArgument<bool> IsValid { get; set; }
+
+        [Category("Output")]
+        public OutArgument<string> OpType { get; set; }
+
+        [Category("Output")]
+        public OutArgument<string> OpEnumType { get; set; }
+
+        [Category("Output")]
+        public OutArgument<string> OpValue { get; set; }
+
+
+        protected override void Execute(CodeActivityContext context)
+        {
+            var type = Type.Get(context);
+            var value = Value.Get(context);
+            DataFieldType datatype = (DataFieldType)Enum.Parse(typeof(DataFieldType), type);
+            var isvalid = CheckValidity(value, datatype);
+            OpType.Set(context, type);
+            OpEnumType.Set(context, datatype);
+            OpValue.Set(context, value);
+            IsValid.Set(context, isvalid);
+        }
+    }
+}
